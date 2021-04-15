c8b1cf11110a1e610dbe2db8e249d05dbf7d85d5
sysjack <60132202+sysjack@users.noreply.github.com>
2021-04-16 01:52:01 +0300

    Update Main.java

--- task_1.1/Main.java
+++ task_1.1/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String firstname = "Dmitry";
         int age = 55:
-        String city ="Moscow"
+        String city = "Moscow"
     }
 }
 0 comments on c

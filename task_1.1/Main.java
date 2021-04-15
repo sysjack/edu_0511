6c5960ff6edc2f803bcb76d1c7efaa4a79f1cc80
sysjack <60132202+sysjack@users.noreply.github.com>
2021-04-16 01:28:03 +0300

    Create Main.java

---
+++ task_1.1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String firstname = "Dmitry";
+        int age =55:
+        String city ="Moscow"
+    }
+}
+0 comments on c

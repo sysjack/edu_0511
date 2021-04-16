ee084f0da8f4101b171bd11417c274b877f425c1
sysjack <60132202+sysjack@users.noreply.github.com>
2021-04-16 03:01:22 +0300

    Create Main.java

---
+++ task_1.5/Main.java
@@ -0,0 +1,21 @@
+/* 
+ВНИМАНИЕ! ДЛЯ РЕШЕНИЯ ЗАДАЧИ НЕОБХОДИМО ПРОСМОТРЕТЬ УРОК №4 https://www.youtube.com/watch?v=WmZ6kVTlWtg&list=PLOep0pi-UBExIxCNegkvCrBuDh0TE7Ihq&index=4
+Программа запрашивает у пользователя значение в градусах Цельсия. Программа должена переводить температуру и выводить на экран
+значение в градусах Фаренгейта.
+Температура по Цельсию TC и температура по Фаренгейту TF связаны следующим соотношением:
+TF = (9 / 5) * TC + 32
+Пример:
+на вход подается значение 41.
+Пример вывода:
+105.8 */
+
+import java.util.Scanner;
+public class Main {
+               public static void main(String[] args) {
+     Scanner scan = new Scanner (System.in);
+     float TC = scan.nextInt();
+     float c = 32;
+     float  TF =  (TC * 9) / 5 + c ;
+        System.out.println( TF );
+    }
+}

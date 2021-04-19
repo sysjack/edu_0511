1bf95fe219663daf097c53c84850f8892263cee4
sysjack <60132202+sysjack@users.noreply.github.com>
2021-04-19 18:49:57 +0300

    Update Main.java

--- task_2.2/Main.java
+++ task_2.2/Main.java
@@ -4,9 +4,28 @@
 3 6 9 12 ...
 4 8 12 16 ...
 ... */
+
+
+
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        int a;
+        int b;
+        int c;
+        a = 1;
+        b = 11;
+        c = 1;
+        while (c<b) {
+            do  {
+                System.out.print((a*c)+ " ");
+                a++;}
+            while (a <b) ;
+            System.out.println();
+            a=1;
+            c++;
+                    };
     }
+
 }
+/* можно сторить таблицу размера b-1 X b-1
+ в условии нет задачи выравнивания столбцов */

ecd2af9223c675386b3a3948377ca1c30342f61e
sysjack <60132202+sysjack@users.noreply.github.com>
2021-04-20 13:13:11 +0300

    Update Main.java

--- task_2.6/Main.java
+++ task_2.6/Main.java
@@ -1,3 +1,16 @@
 /*
 Задание: Даны переменные a и b. Проверьте, что a делится без остатка на b. Если это так - выведите 'Делится' и результат деления, иначе выведите 'Делится с остатком' и остаток от деления.
 */
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print( "Введите первую переменную: ");
+        float a = scan.nextFloat();
+        System.out.print( "Введите вторую переменную: ");
+        float b = scan.nextFloat();
+        if (a%b==0) {
+        System.out.println("Делится "+(a/b)); } else  System.out.println("Делится с остатком "+ a%b);
+    }
+
+}

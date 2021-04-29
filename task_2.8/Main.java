3c6fc4c21f33a12cd9ff681a5cbaa26593ad89c6
sysjack <60132202+sysjack@users.noreply.github.com>
2021-04-29 10:35:52 +0300

    Create Main.java

---
+++ task_2.8/Main.java
@@ -0,0 +1,13 @@
+/*
+Задание: Дан массив с элементами [2, 3, 4, 5]. С помощью цикла for найдите произведение элементов этого массива.
+*/
+public class Main {
+    public static void main (String[] args) {
+        int [] arr={2,3,4,5};
+       int result=1;
+       for (int i=0;i<4;i++) {
+           result*=arr[i];
+        }
+System.out.println("Произведение элементов массива [2,3,4,5] равно "+result);
+    }
+}

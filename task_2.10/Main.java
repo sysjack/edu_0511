69699bc08e18d18677104745852f92941c5b1872
sysjack <60132202+sysjack@users.noreply.github.com>
2021-04-29 11:02:41 +0300

    Create Main.java

---
+++ task_2.10/Main.java
@@ -0,0 +1,15 @@
+/*
+Задание: Дан массив с числами. Проверьте, есть ли в нем два одинаковых числа подряд. Если есть - выведите 'да', а если нет - выведите 'нет'.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+      int [] arr={2,3,4,9,7,7,8,9,10};
+       String result= "Нет";
+       for (int i=0;i<8;i++) {
+           if (arr[i] == arr[i+1]) result="Да";
+        }
+      System.out.println(result);
+    }
+}

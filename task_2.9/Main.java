7e31d6daae9afe5744971621deb18cd63694762e
sysjack <60132202+sysjack@users.noreply.github.com>
2021-04-29 10:58:42 +0300

    Create Main.java

---
+++ task_2.9/Main.java
@@ -0,0 +1,16 @@
+/*
+Задание: Дан массив с числами. Проверьте, что в этом массиве есть число 5. Если есть - выведите 'да', а если нет - выведите 'нет'.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+      int [] arr={2,3,4,7,6,7,8,9,10};
+       String result= "Нет";
+       for (int i=0;i<8;i++) {
+           if (arr[i] == 5) result="Да";
+        }
+    System.out.println(result);
+
+    }
+}

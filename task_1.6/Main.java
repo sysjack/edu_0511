fd205e4941b422f8ee4b064f1b7474b4b371bd59
sysjack <60132202+sysjack@users.noreply.github.com>
2021-05-03 18:04:30 +0300

    Update Main.java

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -4,11 +4,10 @@
 
 import java.util.Scanner;
 public class Main {
-               public static void main(String[] args) {
-     Scanner scan = new Scanner (System.in);
-     float IN = scan.nextInt();
-     float OUT ;
-            OUT =  (IN/100) * 15 + IN ;
-        System.out.println( OUT);
+    public static void main(String[] args) {
+        System.out.print( "Введите число: ");
+        Scanner scan = new Scanner (System.in);
+        double IN = scan.nextDouble();
+        System.out.println( IN+ " плюс 15 % равно " + ((IN/100) * 15 + IN));
     }
 }

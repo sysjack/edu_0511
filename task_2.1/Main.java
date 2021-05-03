4903e1abc90a8a116c6a26882e91652af0041dbf
sysjack <60132202+sysjack@users.noreply.github.com>
2021-05-03 18:52:43 +0300

    Update Main.java

--- task_2.1/Main.java
+++ task_2.1/Main.java
@@ -1,14 +1,23 @@
+  
+/*Вывести на экран все возможные комбинации слов "Мама", "Мыла", "Раму".
+Подсказка: их 6 штук.
+Каждую комбинацию вывести с новой строки. Слова не разделять.
+Пример:
+МылаРамуМама
+РамуМамаМыла
+...
+Требования:
+•	Программа должна выводить текст.
+•	Выведенный текст должен содержать 6 строк.
+•	Текст в каждой строке должен быть уникален.
+•	Должны быть выведены все возможные комбинации. */
 public class Main {
-               public static void main(String[] args) {
-     String a = "мама";
-     String b = "мыла";
-     String c = "раму";
+    public static void main(String[] args) {
+       String s[]={"Мама","Мыла","Раму"};
+       for (int i=0;i<3;i++)
+           for(int j=0;j<3;j++)
+               if (i!=j)
+                   System.out.println( s[i]+s[j]+s[3-i-j]);
 
-        System.out.println( a+b+c);
-                   System.out.println( c+b+a);
-                   System.out.println( a+c+b);
-                   System.out.println( c+c+b);
-                   System.out.println( b+a+c);
-                   System.out.println( b+c+a);
     }
 }

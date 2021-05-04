8196fe2b228aa94e606e664f1cd929ce32830fa7
sysjack <60132202+sysjack@users.noreply.github.com>
2021-05-04 23:08:04 +0300

    Update Main.java

--- task_5.4/Main.java
+++ task_5.4/Main.java
@@ -17,9 +17,28 @@
 8
 10
 */
+import java.util.ArrayList;
+import java.io.*;
+import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args) {
-        // напишите тут ваш код
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader = new BufferedReader(new InputStreamReader((new FileInputStream(fileName))));
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        while (reader.ready()) {
+            int num = Integer.parseInt(reader.readLine());
+            if (num % 2 == 0) {
+                numbers.add(num);
+            }
+        }
+        reader.close();
+        Collections.sort(numbers);
+        for (int i : numbers) {
+            System.out.println(i);
+        }
+
     }
 }

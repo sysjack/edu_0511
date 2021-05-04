409143ffdd5bf1748ac5bd3ee36c4693171e554b
sysjack <60132202+sysjack@users.noreply.github.com>
2021-05-04 13:47:37 +0300

    Update Main.java

--- task_2.8/Main.java
+++ task_2.8/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main (String[] args) {
         int [] arr={2,3,4,5};
        int result=1;
-       for (int i=0;i<4;i++) {
+       for (int i=0;i<arr.length;i++) {
            result*=arr[i];
         }
 System.out.println("Произведение элементов массива [2,3,4,5] равно "+result);

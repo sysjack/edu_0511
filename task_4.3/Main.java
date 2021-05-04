958237bb141749ce39c5fbec1f39b92dff28f8fc
sysjack <60132202+sysjack@users.noreply.github.com>
2021-05-04 21:29:07 +0300

    Update Main.java

--- task_4.3/Main.java
+++ task_4.3/Main.java
@@ -12,12 +12,12 @@
 •	Метод должен возвращать одно и тоже значение, если мы деремся с одним и тем же котом.
 •	Если некий кот1 выигрывает у кота кот2, то кот2 должен проигрывать коту кот1.
 */
-
 public class Main {
     public static void main(String[] args) {
 
     }
 }
+
 
 class Cat {
     public int age;
@@ -28,6 +28,30 @@ class Cat {
     }
 
     public boolean fight(Cat anotherCat) {
-        //напишите тут ваш код
+        if (this.weight != anotherCat.weight || this.age != anotherCat.age || this.strength != anotherCat.strength) {
+            int countThisCat = 0;
+            int countAnotherCat = 0;
+            if (this.age > anotherCat.age) {
+                countThisCat++;
+            } else if (this.age != anotherCat.age)
+                countAnotherCat++;
+            if (this.weight > anotherCat.weight) {
+                countThisCat++;
+            } else if(this.weight != anotherCat.weight){
+                countAnotherCat++;
+            }
+            if (this.strength > anotherCat.strength) {
+                countThisCat++;
+            } else if(this.strength != anotherCat.strength){
+                countAnotherCat++;
+            }
+            if (countThisCat > countAnotherCat) {
+                return true;
+            } else
+                return false;
+        } else
+            return false;
     }
+
+
 }

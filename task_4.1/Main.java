23f0d4f5b41a62d78b3923aca0574f04d4f159f0
sysjack <60132202+sysjack@users.noreply.github.com>
2021-05-04 20:57:38 +0300

    Update Main.java

--- task_4.1/Main.java
+++ task_4.1/Main.java
@@ -11,15 +11,19 @@ public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat();
         cat.setName("Жужик");
-        System.out.println(cat.name);
+       /* System.out.println(cat.name);*/
     }
 }
 
 class Cat {
     private String name = "безымянный кот";
 
-    public void setName(String name) {
-        //напишите тут ваш код
+    public void setName(String name){
+                this.name=name;
+        System.out.println(name);
+        }
 
-    }
 }
+
+
+

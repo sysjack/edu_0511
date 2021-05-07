482f9b9977d67b7b637d2b1e66fb4ae28742bd7f
sysjack <60132202+sysjack@users.noreply.github.com>
2021-05-08 00:29:28 +0300

    Update Main.java

--- task_2.11/Main.java
+++ task_2.11/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main (String[] args) {
         int [] arr={19,3,1,1,3,7,8,9,10};
             int summ = 0;
-            for (int i = 0; i < 8; i++) {
+            for (int i = 0; i < arr.length; i++) {
                 summ += arr[i];
                 if (summ >=11) {
                     System.out.println("Чтобы сумма получилось больше 10, надо сложить " + (i + 1) + " элементов массива.");

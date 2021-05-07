aacfb3673fc550095fdd4fe36fe6847a60acbf28
sysjack <60132202+sysjack@users.noreply.github.com>
2021-05-08 00:30:54 +0300

    Update Main.java

--- task_2.10/Main.java
+++ task_2.10/Main.java
@@ -7,7 +7,7 @@ public class Main {
         //напишите тут ваш код
       int [] arr={2,3,4,9,7,7,8,9,10};
        String result= "Нет";
-       for (int i=0;i<8;i++) {
+       for (int i=0;i<arr.length;i++) {
            if (arr[i] == arr[i+1]) result="Да";
         }
       System.out.println(result);
